d67e8642f26b5b960d051a442cd2ed59bc78b511
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-01-06 08:40:48 -0600

    Don't include chatterDisplayName in response when the chatter is the bot

--- Wobble.Models/ChatCommands/ChatCommand.cs
+++ Wobble.Models/ChatCommands/ChatCommand.cs
@@ -25,6 +25,12 @@ namespace Wobble.Models.ChatCommands
                 return "";
             }
 
+            // Don't include chatterDisplayName in response when the chatter is the bot
+            if (chatterDisplayName.Equals(botDisplayName))
+            {
+                return _responses[RandomNumberGenerator.NumberBetween(0, _responses.Count - 1)];
+            }
+
             return chatterDisplayName + " " +
                    _responses[RandomNumberGenerator.NumberBetween(0, _responses.Count - 1)];
         }
